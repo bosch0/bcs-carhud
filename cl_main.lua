c96609eeb428338b99fa53b938233bc07f77ddab
B O S C H <116667373+bosch0@users.noreply.github.com>
2023-07-22 18:42:01 +0200

    Update cl_main.lua

--- cl_main.lua
+++ cl_main.lua
@@ -1,31 +1,34 @@
+local fuelSystem = string.upper(CarHUD.FuelSystem)
+
 -- @main
 
 CreateThread(function ()
     while true do
         local player = PlayerPedId()
-	local vehicle = GetVehiclePedIsIn(player) 
-	local fuelSystem = string.upper(CarHUD.FuelSystem)
-	if (fuelSystem == "LEGACYFUEL") then
-		fuelLevel = exports['LegacyFuel']:GetFuel(vehicle)
-	elseif (fuelSystem == "NDFUEL") then
-		fuelLevel = exports['ND_Fuel']:GetFuel(vehicle) -- MUST ADD EXPORT IN NDFUEL SCRIPT
-	elseif (fuelSystem == "DEFAULT") then
-		fuelLevel = GetVehicleFuelLevel(vehicle)
-	elseif (fuelSystem == "CUSTOM") then
-		-- Add Here the custom code
-		-- local fuelLevel = (CUSTOM CODE FOR GET FUEL)
-	end
-	local fuel = math.floor(fuelLevel)
-        if CarHUD.type == 'mp/h' then
-            vel = math.floor(GetEntitySpeed(vehicle) * 2.236936)
-        elseif CarHUD.type == 'km/h' then
-            vel = math.floor(GetEntitySpeed(vehicle) * 3.6)
-        else
-            print('^3[BCS-carhud]^0 check CarHUD.type in Config.lua')
-            vel = nil
-        end
+        local vehicle = GetVehiclePedIsIn(player)
         if not IsPauseMenuActive() and IsPedInAnyVehicle(player) and not IsThisModelABicycle(vehicle) then
             sleep = 50
+            
+            if CarHUD.type == 'mp/h' then
+                vel = math.floor(GetEntitySpeed(vehicle) * 2.236936)
+            elseif CarHUD.type == 'km/h' then
+                vel = math.floor(GetEntitySpeed(vehicle) * 3.6)
+            else
+                print('^3[BCS-carhud]^0 check CarHUD.type in Config.lua')
+                vel = nil
+            end
+            if (fuelSystem == "LEGACYFUEL") then
+                fuelLevel = exports['LegacyFuel']:GetFuel(vehicle)
+            elseif (fuelSystem == "NDFUEL") then
+                fuelLevel = exports['ND_Fuel']:GetFuel(vehicle) -- MUST ADD EXPORT IN NDFUEL SCRIPT
+            elseif (fuelSystem == "DEFAULT") then
+                fuelLevel = GetVehicleFuelLevel(vehicle)
+            elseif (fuelSystem == "CUSTOM") then
+                -- Add Here the custom code
+                -- local fuelLevel = (CUSTOM CODE FOR GET FUEL)
+            end
+            local fuel = math.floor(fuelLevel)
+
             SendNUIMessage({
                 toggle = true,
                 vel = vel,
